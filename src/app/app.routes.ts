ef4d2f2d4494d38fd77a7aa2001afd2cb9c360d5
GuillermoMol1na <guillermo.m@krugerns.com>
2024-03-19 18:19:13 -0400

    First commit of the Angular proj

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SquareComponent } from './square/square.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        component: HomeComponent,
+        
+    },
+    {
+        path: 'square',
+        component: SquareComponent,
+    }
+];
---
+++ src/app/square/square.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './square.component.html',
+  styleUrl: './square.component.css'
+})
+export class SquareComponent {
+
+}
